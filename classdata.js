4aed93ec26cb7e4e3820a725a7deb2d9a3f3fe1a
arnoy-joking <arnoy799@gmail.com>
2025-05-14 23:51:55 +0600

    classdata.js

---
+++ classdata.js
@@ -0,0 +1,147 @@
+// Class data for the chemistry course
+const classData = [
+  {
+    videoId: "RUQcm3vIbWo",
+    title: "Course Roadmap",
+    pdfLink: "20240527-1716748184-LRH05IsN.pdf",
+    duration: "45:12",
+    date: "May 27, 2024",
+    category: "Course Roadmap"
+  },
+  {
+    videoId: "CLrwwQj697Q",
+    title: "Basic Building Class (BBC)-01",
+    pdfLink: "20240529-1716937235-9lpGk0ak.pdf",
+    duration: "52:30",
+    date: "May 29, 2024",
+    category: "BBC Classes"
+  },
+  {
+    videoId: "P-dkM5_I1VU",
+    title: "Basic Building Class (BBC)-02",
+    pdfLink: "20240601-1717261772-VB8HeJSt.pdf",
+    duration: "48:15",
+    date: "Jun 01, 2024",
+    category: "BBC Classes"
+  },
+  {
+    videoId: "pxsGAGomnoI",
+    title: "Basic Building Class (BBC)-03",
+    pdfLink: "20240603-1717361228-TPilcyPf.pdf",
+    duration: "55:20",
+    date: "Jun 03, 2024",
+    category: "BBC Classes"
+  },
+  {
+    videoId: "p3wBA8no6c4",
+    title: "Basic Building Class (BBC)-04",
+    pdfLink: "20240605-1717589649-Qsqg5u7r.pdf",
+    duration: "50:45",
+    date: "Jun 05, 2024",
+    category: "BBC Classes"
+  },
+  {
+    videoId: "JJuvBfrGUVo",
+    title: "Basic Building Class (BBC)-05",
+    pdfLink: "20240607-1717710084-HL1UVZIy.pdf",
+    duration: "49:30",
+    date: "Jun 07, 2024",
+    category: "BBC Classes"
+  },
+  {
+    videoId: "WlrST4v-kaI",
+    title: "Basic Building Class (BBC)-06",
+    pdfLink: "20240612-1718138903-CosRm2Hp.pdf",
+    duration: "53:15",
+    date: "Jun 12, 2024",
+    category: "BBC Classes"
+  },
+  {
+    videoId: "cUM3EbhlMmU",
+    title: "Basic Building Class (BBC)-07",
+    pdfLink: "20240614-1718313883-TerlymZ6.pdf",
+    duration: "51:40",
+    date: "Jun 14, 2024",
+    category: "BBC Classes"
+  },
+  {
+    videoId: "_KsZlI7wwBo",
+    title: "গুণগত রসায়ন পর্ব-০১",
+    pdfLink: "20240701-1719779817-aIxobbwK.pdf",
+    duration: "56:08",
+    date: "Jul 01, 2024",
+    category: "গুণগত রসায়ন"
+  },
+  {
+    videoId: "u9-QXqsswyk",
+    title: "গুণগত রসায়ন পর্ব-০২",
+    pdfLink: "20240703-1719944077-MSjIfz7g.pdf",
+    duration: "49:20",
+    date: "Jul 03, 2024",
+    category: "গুণগত রসায়ন"
+  },
+  {
+    videoId: "IGa_6J-VB3o",
+    title: "গুণগত রসায়ন পর্ব-০৩",
+    pdfLink: "20240705-1720129590-Kws5CXLp.pdf",
+    duration: "52:10",
+    date: "Jul 05, 2024",
+    category: "গুণগত রসায়ন"
+  },
+  {
+    videoId: "cOIPzY3ddJU",
+    title: "গুণগত রসায়ন পর্ব-০৪",
+    pdfLink: "20240708-1720385932-6hGrw8gr.pdf",
+    duration: "48:35",
+    date: "Jul 08, 2024",
+    category: "গুণগত রসায়ন"
+  },
+  {
+    videoId: "hWVHKkx2FJs",
+    title: "গুণগত রসায়ন পর্ব-০৫",
+    pdfLink: "20240710-1720616665-v7M9BxDL.pdf",
+    duration: "54:20",
+    date: "Jul 10, 2024",
+    category: "গুণগত রসায়ন"
+  },
+  {
+    videoId: "4aVBQzg5L80",
+    title: "গুণগত রসায়ন পর্ব-০৬",
+    pdfLink: "20240715-1720997756-tIbXPU3s.pdf",
+    duration: "55:45",
+    date: "Jul 15, 2024",
+    category: "গুণগত রসায়ন"
+  },
+  {
+    videoId: "GE4dDmImPSA",
+    title: "গুণগত রসায়ন পর্ব-০৭",
+    pdfLink: "20240820-1724176154-tJTn6aKB.pdf",
+    duration: "50:55",
+    date: "Aug 20, 2024",
+    category: "গুণগত রসায়ন"
+  },
+  {
+    videoId: "IaKl2mIISw8",
+    title: "গুণগত রসায়ন পর্ব-০৮",
+    pdfLink: "20240828-1724790082-jt5KjqbB.pdf",
+    duration: "47:30",
+    date: "Aug 28, 2024",
+    category: "গুণগত রসায়ন"
+  },
+  {
+    videoId: "JMeG_IB_Yvs",
+    title: "গুণগত রসায়ন পর্ব-০৯",
+    pdfLink: "20240830-1724967108-BAaCtxU5.pdf",
+    duration: "53:25",
+    date: "Aug 30, 2024",
+    category: "গুণগত রসায়ন"
+  },
+  {
+    videoId: "ZnHMZAGrIj8",
+    title: "গুণগত রসায়ন পর্ব-১০",
+    pdfLink: "20240902-1725216596-9uStRcWl.pdf",
+    duration: "51:15",
+    date: "Sep 02, 2024",
+    category: "গুণগত রসায়ন"
+  }
+];
